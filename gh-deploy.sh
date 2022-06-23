937e7f241e435a9ff1dc2e650dcc249b2fe5b089
James Perrin <countrydawgg@gmail.com>
2022-06-23 09:32:25 -0700

    Add GH Pages deploy script

---
+++ gh-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# See: https://gist.github.com/cobyism/4730490
+
+if [ -z "$1" ]
+then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+git subtree push --prefix $1 origin gh-pages
